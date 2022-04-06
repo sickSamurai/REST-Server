111c29c933e3f82cdd8dbd1271a23b6d71607aff
Laurent Chaverra <42363109+sickSamurai@users.noreply.github.com>
2022-04-06 00:46:11 -0500

    Updates Menores

--- models/Server.js
+++ models/Server.js
@@ -1,29 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const { connectDB } = require("../database/config")
+const express = require('express')
+const cors = require('cors')
+const { connectDB } = require('../database/config')
 
 class Server {
    constructor() {
       this.app = express()
       this.port = process.env.PORT
-      this.userPath = "/api/users"
+      this.userPath = '/api/users'
       this.middlewares()
       this.configRoutes()
-      connectDB()
-   }
-
-   listen() {
-      this.app.listen(this.port, () => console.log(`Servidor corriendo en el puerto ${this.port}`))
    }
 
    middlewares() {
       this.app.use(cors())
       this.app.use(express.json())
-      this.app.use(express.static("public"))
+      this.app.use(express.static('public'))
    }
 
    configRoutes() {
-      this.app.use(this.userPath, require("../routes/userRoutes"))
+      this.app.use(this.userPath, require('../routes/userRoutes'))
+   }
+
+   listen() {
+      this.app.listen(this.port, () => console.log(`Servidor corriendo en el puerto ${this.port}`))
+      connectDB()
    }
 }
 
